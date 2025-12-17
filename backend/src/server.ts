e972827c97bf6c91bbc945473a9d48768e18abe4
Sreeram <sreeram@github.com>
2025-12-17 15:21:33 +0530

    cors changed again

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -48,7 +48,7 @@ app.use(morgan("dev"))
 app.use(cookieParser());
 app.use(
   cors({
-    origin:["http://localhost:5173","https://cure-connect-main.vercel.app"],
+    origin:["http://localhost:5173","https://cure-connect-main-1.onrender.com"],
     credentials: true,
   })
 );
